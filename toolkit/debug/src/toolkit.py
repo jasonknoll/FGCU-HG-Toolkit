22a861ff43af1a401a89680acffca22f1deb2465
Jason Knoll <jasonknoll98@aol.com>
2021-09-12 18:00:14 -0400

    Update toolkit.py

--- toolkit/debug/src/toolkit.py
+++ toolkit/debug/src/toolkit.py
@@ -3,6 +3,24 @@
 Created on Sun Sep 12 17:30:35 2021
 
 @author: jason
+
+First tool: allow user (research team member) to automate
+their data entry. Eventually leading to automated data
+visualizations to be used in presentations.
 """
 
-# My real code will go in here
+from openpyxl import Workbook, load_workbook
+from openoyxl.utils import get_column_letter
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+ Main function will be our main loop running everything
+ including the gui
+"""
+def main():
+    pass 
+
+if __name__ == '__main__':
+    main()
