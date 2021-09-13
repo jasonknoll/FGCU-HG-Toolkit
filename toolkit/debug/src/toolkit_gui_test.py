3d7cf6993f3c4ccdd943e9888ad563d93379ecfc
Jason Knoll <jasonknoll98@aol.com>
2021-09-13 18:15:26 -0400

    Writing notes to myself

--- toolkit/debug/src/toolkit_gui_test.py
+++ toolkit/debug/src/toolkit_gui_test.py
@@ -4,12 +4,26 @@ Created on Mon Sep 13 10:57:47 2021
 
 @author: jason
 """
+
 import tkinter
+from openpyxl import Workbook, load_workbook 
+
 
 """
  TODO everything 
+ + big buttons for sheet control
+     - create
+     - open
+     - save 
+ + if sheet is open, have buttons/menu to edit cells
+ + deal with the *event binding*
 """
 
+# Main loop will hold my "global" varaibles
+def main():
+    # Blank default workbook
+    current_wb = Workbook()
+    current_ws = current_wb.active
 
 if __name__ == '__main__':
     pass
