1ebc6cb609a0c9de967d33f1a57d5f1edc73da3c
Jason Knoll <jasonknoll98@aol.com>
2021-09-27 18:00:16 -0400

    Update gui.py

--- toolkit/debug/auto_well_data/gui.py
+++ toolkit/debug/auto_well_data/gui.py
@@ -2,3 +2,32 @@
  Handle the GUI and interactions
  with the user
 """
+
+from tkinter import *, ttk
+
+# TODO Setup window size
+# Add buttons and shit
+# Figure out how to select files from HD
+# connect to Google Sheet API
+
+WINDOW_WIDTH = 0
+WINDOW_HEIGHT = 0
+WINDOW_TITLE = "Hydrogeology Well Data Processor v0.1"
+
+# I have to setup a canvas or something
+
+# Combine old data with the newly generated sheet
+def combine_sheets(sheet1, sheet2):
+ pass
+
+def select_file():
+ pass
+
+def draw_window():
+ pass
+
+def main():
+ pass
+
+if __name__ == "__main__":
+ main()
