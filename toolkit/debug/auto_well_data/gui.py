fb20f33e7862a06bd59622ca98d69bc5858f2658
Jason Knoll <jasonknoll98@aol.com>
2021-10-18 17:59:34 -0400

    Setup Buttons

--- toolkit/debug/auto_well_data/gui.py
+++ toolkit/debug/auto_well_data/gui.py
@@ -10,6 +10,8 @@ import html_reader as hr
 
 import sheets_manager as sm
 
+# import openpyxl
+
 # TODO Setup window size
 # Add buttons and shit
 # Figure out how to select files from HD
@@ -18,8 +20,8 @@ import sheets_manager as sm
 # Setup our window
 root = Tk()
 
-html_reader = hr.HtmlReader
-sheets_manager = sm.SheetsManager
+html_reader = hr.HtmlReader()
+sheets_manager = sm.SheetsManager()
 
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
@@ -30,12 +32,21 @@ connected_to_sheets = False
 
 # Setup the buttons
 # TODO need to setup locations for everything
-select_html_file_button = Button(root, text="b1", command=None)
-select_manual_well_data_button = Button(root, text="b2")
-generate_sheet_button = Button(root, text="b2")
+select_html_file_button = Button(root, text="Upload HTML File", command=None, padx=20, pady=20)
+select_html_file_button.grid(row=0, column=0)
+
+select_manual_well_data_button = Button(root, text="Upload Manual Well Measurement", padx=20, pady=20)
+select_manual_well_data_button.grid(row=0, column=1)
+
+generate_sheet_button = Button(root, text="Generate New Sheet", padx=20, pady=20)
+generate_sheet_button.grid(row=0, column=2)
+
+# Handle all things necessary to connect to google account
+def connect_to_sheets():
+    pass
 
 def main():
-    pass
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
