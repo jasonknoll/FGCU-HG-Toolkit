7c63a0f2f3d35d6a3f120fa0520475ed7c630c9a
Jason Knoll <jasonknoll98@aol.com>
2021-10-22 22:46:47 -0400

    Improved Pandas Speed

--- toolkit/debug/auto_well_data/sheets_manager.py
+++ toolkit/debug/auto_well_data/sheets_manager.py
@@ -8,55 +8,42 @@
 import pandas as pd
 from openpyxl import Workbook, load_workbook
 
-# Create objects for every well to organize the data a little better
-class Well:
-    def __init__(self, name=''):
-        self.name = name
-        self.isAutomatic = False
-        if (self.name[-1].upper() == 'A'):
-            self.isAutomatic = True
-            
 
 class SheetsManager:
     def __init__(self, s1=None, s2=None):
         # old and new sheets needing to be compiled together
-        self.manual_data = s1
+        self.manual_data_path = s1
         self.existing_well_data = s2
         self.new_sheet = None
         
-        self.curr_wb = None
+        self.manual_wb = None
         
+        # Pandas dataframes
         self.wells = []
-        
-        # pandas splits all the dataframes into sheets for us? https://www.sitepoint.com/using-python-parse-spreadsheet-data/
-        self.sheets = []
+
         
     def set_manual_data_path(self, sheet):
-        self.manual_data = sheet
+        self.manual_data_path = sheet
         
     def get_manual_data_path(self):
-        return self.manual_data
-    
-    def check_if_well_exists(self, well_name) -> bool:
-        for w in self.wells:
-            if (w.name != well_name):
-                    continue
-            else:
-                return True
-        return False
+        return self.manual_data_path
             
     
     # TODO read all data from the manual entries
     def scrape_manual_sheet(self):
         # First it'll scrape all the sheets to get the wells and create our well objects
-        self.curr_wb = load_workbook(self.get_manual_data_path())
+        self.manual_wb = pd.ExcelFile(self.get_manual_data_path())
         
         # Create our well objects from file without creating duplicates
-        for s in self.curr_wb.sheetnames:
-            if (not self.check_if_well_exists(s)):
-                self.wells.append(Well(s))
-                
-        print(self.curr_wb[self.wells[0].name]['A1'].value)
+        
+        for s in self.manual_wb.sheet_names:
+            self.wells.append(pd.read_excel(self.manual_wb, s))
+            #self.wells.append(pd.read_excel(self.get_manual_data_path(), sheet_name=s))
+            # Maybe have a popup window showing progress?? 
+            
+        #print(self.wells[0].head()) 
+        #print(self.wells[0]['7A I (mm)'])
+        
             
         # TODO search through all the rows and columns and put it all into a 2d list?
         # Create pandas dataframe and work with it?
