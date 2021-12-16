b37fbdc92a757363a06b993d8cf24b0191529ea3
Jason Knoll <jasonknoll98@aol.com>
2021-12-15 22:04:25 -0500

    Create grapher.py

---
+++ toolkit/debug/auto_well_data/grapher/grapher.py
@@ -0,0 +1,24 @@
+"""
+ Project outline for Well data sheets grapher 
+
+ Due: 01/08/2022
+
+ Requirements:
+ - Using the UI the user can:
+    + Select date range
+    + Select wells 
+ - Generate graph 
+    + generate multiple/more complex grpahs in the future    
+"""
+
+# Use Kivy as a UI (more basic user friendly)
+# Learn kivy lmao
+
+# Need to get google sheets setup again. That shit was annoying 
+# but I gotta do it
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
