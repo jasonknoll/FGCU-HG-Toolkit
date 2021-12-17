39593e9e60618fbf40ba7354ef7bcebb7816e69a
Jason Knoll <jasonknoll98@aol.com>
2021-12-17 00:33:08 -0500

    Update grapher.py

--- toolkit/debug/auto_well_data/grapher/grapher.py
+++ toolkit/debug/auto_well_data/grapher/grapher.py
@@ -7,7 +7,7 @@
  - Using the UI the user can:
     + Select date range
     + Select wells 
- - Generate graph 
+ - Generate graph from selection 
     + generate multiple/more complex grpahs in the future    
 """
 
@@ -16,6 +16,15 @@
 
 # Need to get google sheets setup again. That shit was annoying 
 # but I gotta do it
+
+"""
+ TODO
+ - Setup development enviornment on laptop
+ - Map out requirements
+ - Design module (single file probably)
+ - Test module 
+ - Package module for team
+"""
 
 def main():
     pass
