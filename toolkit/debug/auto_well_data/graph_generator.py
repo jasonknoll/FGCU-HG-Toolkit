013d35b92c2d69d71439f144269de3a89924def6
Jason Knoll <jasonknoll98@aol.com>
2021-12-21 11:28:40 -0700

    Created graph generator file

---
+++ toolkit/debug/auto_well_data/graph_generator.py
@@ -0,0 +1,13 @@
+"""
+ TODO
+ Setup google sheets connection
+ Download selected sheets as dataframes (if slow figure out alternatives)
+ Create a GUI to allow the user to make selections (kivy?)
+ Generate graphs based off of user parameters
+"""
+
+def main():
+    pass 
+
+if __name__ == '__main__':
+    main()
