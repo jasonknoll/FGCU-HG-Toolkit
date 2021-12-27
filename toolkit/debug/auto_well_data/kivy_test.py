9e101b9fdafd6fdda09db0c4550c41eee8249dd8
Jason Knoll <jasonknoll98@aol.com>
2021-12-27 12:19:29 -0700

    Created test login

--- toolkit/debug/auto_well_data/kivy_test.py
+++ toolkit/debug/auto_well_data/kivy_test.py
@@ -2,10 +2,26 @@ import kivy
 
 from kivy.app import App 
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput 
+from kivy.uix.gridlayout import GridLayout
+
+
+class LoginScreen(GridLayout):
+    def __init__(self, *args, **kwargs):
+        super(LoginScreen, self).__init__(*args, **kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='Username'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='Password'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
 
 class KivyApp(App):
+
+    # root widgets
     def build(self):
-        return Label(text='Hello world')
+        return LoginScreen()
 
 
 if __name__ == '__main__':
