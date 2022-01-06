528050c63c3bbff0dc249be0bae5b41c06d8ca9d
Jason Knoll <jasonknoll98@aol.com>
2022-01-06 16:11:23 -0700

    Login button works

--- toolkit/debug/well_grapher/src/well_grapher.py
+++ toolkit/debug/well_grapher/src/well_grapher.py
@@ -51,69 +51,8 @@ Config.write()
 red_text_color = 'ff0000'
 green_text_color = '00ff00'
 
-
-"""
- + GraphGenerator
- The graph genertor object will take in which sheets and colors have been selected by the GUI
- and parse those sheets. The data will be inserted into pandas dataframes which will
- allow for easier working with matplotlib.
-"""
-class GraphGenerator:
-    def __init__(self):
-        pass
-    
-    def generate(self):
-        pass
-
-
-"""
- Main menu window (maybe do a free float layout?)
-"""
-class MainMenu(GridLayout):
-    def __init__(self, *args, **kwargs):
-        super(MainMenu, self).__init__(*args, **kwargs)
-
-        # check for credentials/token to determine this
-        # REMOVE THIS FROM CLASS POTENTIALLY
-        # SOME OF THESE VARIABLES NEEDS TO BE ACCESSED/EDITED OUTSIDE OF THIS CLASS
-        self.logged_in = False
-
-        self.cols = 1
-
-        self.logged_in_label = Label(text=f'Connected to Google Sheets: [color={red_text_color}]{str(self.logged_in)}[/color]', markup=True)
-        
-        # Using kv language, update label
-        self.add_widget(self.logged_in_label)
-
-        # Maybe gray-out if logged in already?
-        self.login_button = Button(text='Login')
-        self.add_widget(self.login_button)
-
-        # On click, open the graphing menu
-        self.graph_menu_button = Button(text='Graph menu')
-        self.add_widget(self.graph_menu_button)
-
-    def set_login(self):
-        return not self.loggin_in
-
-
-
-"""
- Graphing menu window
-"""
-class GraphMenu(GridLayout):
-    def __init__(self):
-        pass
-
-
-"""
- Main application object used by kivy to run the app
-"""
-class GraphApp(App):
-    def build(self):
-        Window.size = (640, 480)
-        self.title = 'FGCU Hydrogeology Graph Generator'
-        return MainMenu()
+g_scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+sheet_id = '1u8uMAEu6FZPHEoKERau1R_emPtARuAPBbDWfZZvY6Ao'
 
 
 """
@@ -128,6 +67,7 @@ class GoogleHandler:
     """
     @staticmethod
     def connect_to_google(scopes):
+        creds = ""
         if os.path.exists('../creds/token.json'):
             creds = Credentials.from_authorized_user_file('../creds/token.json', scopes)
         # If there are no (valid) credentials available, let the user log in.
@@ -153,12 +93,92 @@ class GoogleHandler:
         return build('sheets', 'v4', credentials=creds).spreadsheets()
 
 
+
+"""
+ + GraphGenerator
+ The graph genertor object will take in which sheets and colors have been selected by the GUI
+ and parse those sheets. The data will be inserted into pandas dataframes which will
+ allow for easier working with matplotlib.
+"""
+class GraphGenerator:
+    def __init__(self):
+        pass
+    
+    def generate(self):
+        pass
+
+
+"""
+ Main menu window (maybe do a free float layout?)
+"""
+class MainMenu(GridLayout):
+    def __init__(self, goog=GoogleHandler(), *args, **kwargs):
+        super(MainMenu, self).__init__(*args, **kwargs)
+
+        # check for credentials/token to determine this
+        # REMOVE THIS FROM CLASS POTENTIALLY
+        # SOME OF THESE VARIABLES NEEDS TO BE ACCESSED/EDITED OUTSIDE OF THIS CLASS
+        self.logged_in = False
+
+        self.google = goog
+
+        self.cols = 1
+
+        self.logged_in_label = Label(text=f'Connected to Google Sheets: [color={red_text_color}]{str(self.logged_in)}[/color]', markup=True)
+        
+        # Using kv language, update label
+        self.add_widget(self.logged_in_label)
+
+        # Maybe force the user to do this every time
+        self.login_button = Button(text='Login')
+        self.login_button.bind(on_press=self.login)
+        self.add_widget(self.login_button)
+
+        # On click, open the graphing menu
+        self.graph_menu_button = Button(text='Graph menu')
+        self.add_widget(self.graph_menu_button)
+
+    # Probably not very useful
+    def set_login(self):
+        return not self.loggin_in
+
+    """
+     Used by the button to actually login using the global scopes variable
+    """
+    def login(self, instance):
+        self.google.connect_to_google(g_scopes)
+
+    def test_click(self, instance):
+        print("ello govna")
+
+
+
+
+"""
+ Graphing menu window
+"""
+class GraphMenu(GridLayout):
+    def __init__(self):
+        pass
+
+
+"""
+ Main application object used by kivy to run the app
+"""
+class GraphApp(App):
+    def build(self):
+        Window.size = (640, 480)
+        self.title = 'FGCU Hydrogeology Graph Generator'
+        return MainMenu()
+
+
+
+
 def main():
-    scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-    sheet_id = '1u8uMAEu6FZPHEoKERau1R_emPtARuAPBbDWfZZvY6Ao'
+    
     #creds = None
 
-    goog = GoogleHandler()
+    #goog = GoogleHandler()
     """
      Workflow: user hits login, if not logged in already, then the user will be able to hit
      the graphing button which opens up the graphing menu. 
