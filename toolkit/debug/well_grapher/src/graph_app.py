e13aaa97c0104d46edbc48253bd13cbb13e11377
Jason Knoll <jasonknoll98@aol.com>
2022-01-25 11:44:55 -0500

    Started refactoring

---
+++ toolkit/debug/well_grapher/src/graph_app.py
@@ -0,0 +1,19 @@
+"""
+ FGCU Hydrogeology well data grapher app interface
+ Author: Jason Knoll
+"""
+
+# Imports
+from kivy.app import App
+
+# Kivy Interface Instance class
+class GraphApp(App):
+    def build(self):
+        pass
+
+# Main function that gets called on startup
+def main():
+    GraphApp().run()
+
+if __name__ == '__main__':
+    main()
---
+++ toolkit/debug/well_grapher/src/graph_generator.py
@@ -0,0 +1,15 @@
+"""
+ FGCU Hydrogeology graph generator 
+ Author: Jason Knoll
+"""
+
+# Imports
+import pandas as pd
+
+"""
+ generate_graph
+ 
+ 
+"""
+def generate_graph(df: List[pd.DataFrame]):
+    pass
