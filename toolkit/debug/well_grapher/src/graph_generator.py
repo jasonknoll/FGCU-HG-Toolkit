0f376cf6af28d52141513975bd794b577a4bff9e
Jason Knoll <jasonknoll98@aol.com>
2022-01-27 11:33:55 -0500

    Added get last row function

--- toolkit/debug/well_grapher/src/graph_generator.py
+++ toolkit/debug/well_grapher/src/graph_generator.py
@@ -6,6 +6,10 @@
 # Imports
 import pandas as pd
 
+from typing import List
+
+
+# Module functions
 """
  generate_graph
  
@@ -14,3 +18,16 @@ import pandas as pd
 """
 def generate_graph(df: List[pd.DataFrame]):
     pass
+
+"""
+ get_last_row
+ 
+ @desc: Find the last row in the sheet
+ @return: index of the next row available in the sheet
+"""
+def get_last_row(self, well):
+    rows = self.sheet.values().get(spreadsheetId=sheet_id,
+                            range=f"{well}!A1:200").execute()
+    data = rows.get('values')
+
+    return len(data)+1
