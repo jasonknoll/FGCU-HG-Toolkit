1077bc9c53f0b07a5bf19e93a5385ae95f64bdac
Jason Knoll <jasonknoll98@aol.com>
2022-01-27 11:39:36 -0500

    Update graph_generator.py

--- toolkit/debug/well_grapher/src/graph_generator.py
+++ toolkit/debug/well_grapher/src/graph_generator.py
@@ -20,14 +20,23 @@ def generate_graph(df: List[pd.DataFrame]):
     pass
 
 """
- get_last_row
+ get_last_row (google sheets)
  
  @desc: Find the last row in the sheet
  @return: index of the next row available in the sheet
 """
-def get_last_row(self, well):
-    rows = self.sheet.values().get(spreadsheetId=sheet_id,
+def get_last_row(sheet, well):
+    rows = sheet.values().get(spreadsheetId=sheet_id,
                             range=f"{well}!A1:200").execute()
     data = rows.get('values')
 
     return len(data)+1
+
+"""
+ create_dfs_from_sheets
+
+ @desc: From google sheets, generate dataframes that will be used to generate graphs
+ @return: list of dataframes
+"""
+def create_dfs_from_sheet():
+    pass
