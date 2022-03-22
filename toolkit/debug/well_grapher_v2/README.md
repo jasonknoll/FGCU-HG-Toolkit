0b157e9479ff67ebf60ead6d463d1d7f9bc11878
Jason Knoll <jasonknoll98@aol.com>
2022-03-22 18:41:01 -0400

    blah

--- toolkit/debug/well_grapher_v2/README.md
+++ toolkit/debug/well_grapher_v2/README.md
@@ -1,3 +1,13 @@
 # Hydrogeology Well Data Grapher V2.0
 
-*This is a major refactoring of the first version of the well data grapher.*
+*This is a major refactoring of the first version of the well data grapher*
+
+## Introduction
+---
+
+## Major Components
+---
+### Login Menu
+
+
+### Graph Menu
--- toolkit/debug/well_grapher_v2/src/app.py
+++ toolkit/debug/well_grapher_v2/src/app.py
@@ -2,11 +2,14 @@
  This is the main file which houses the GUI and main loop
 '''
 
+import os
+import platform
+
 import kivy
 
-from kivy.app import App 
+from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput 
+from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.checkbox import CheckBox
@@ -22,25 +25,53 @@ from kivy.core.window import Window
 from kivy.properties import ObjectProperty
 
 
-class GraphMenu(Screen): 
+# Google functions
+
+
+class GraphMenu(Screen):
     ''' 
      This is the class for the object which contains all UI data for the graph menu GUI
     '''
     pass
 
-class LoginMenu(Screen): 
+
+class LoginMenu(Screen):
     '''
      This contains the login menu, which consists of only one widget:
      a button that will call google's login snippet. 
 
      This screen should be skipped if credentials are valid
     '''
+    def __init__(self, *args, **kwargs): 
+        # init Screen super class
+        super(LoginMenu, self).__init__(*args, **kwargs)
+
+
+"""
+ TODO need to check if the creds are valid. If not,
+ delete them and set a `logged_in` variable to false
+"""
+def delete_creds():
     pass
 
-class GraphGenerator(App):
-    sm = ScreenManager() 
+def check_creds_valid() -> bool:
+    '''       
+    '''
+    pass
 
-    #sm.add_widget()
+
+class GraphGenerator(App):
+    '''
+     Kivy object which contains our app widgets
+    '''
+    sm = ScreenManager()
+
+    # sm.add_widget()
+
+
+def main():
+    GraphGenerator().run()
+
 
 if __name__ == '__main__':
     pass
